504d5c3f94dcb1369e069a51b50cf012d4b98fbc
gaurav <gaurav@uengage.in>
2022-06-09 23:18:30 +0530

    contact page change

--- src/data.js
+++ src/data.js
@@ -1,33 +1,40 @@
 export const products = [
-  {
-    id: 1,
-    img: "/images/github.png",
-    link: "https://github.com/gauravkataria21",
-    name: "Github-Link",
-  },
-  {
-    id: 2,
-    img: "/images/blog.png",
-    link: "https://tech-bloggy-app.herokuapp.com/",
-    name: "Tech-Blog",
-  },
-  {
-    id: 3,
-    img: "/images/pizza.png",
-    link: "https://pizzas-restro.herokuapp.com/",
-    name: "React-Pizza",
-  },
-  {
-    id: 4,
-    img: "/images/web.png",
-    link: "https://webhose-cloud-hosting.netlify.app/index.html",
-    name: "Webhose.",
-  },
+	{
+		id: 1,
+		img: "/images/github.png",
+		link: "https://github.com/gauravkataria21",
+		name: "Github-Link",
+	},
+	{
+		id: 2,
+		img: "/images/blog.png",
+		link: "https://tech-bloggy-app.herokuapp.com/",
+		name: "Tech-Blog",
+	},
+	{
+		id: 3,
+		img: "/images/outfit.png",
+		link: "https://outfit-delight.netlify.app/",
+		name: "Outfit-Delight",
+	},
 
-  {
-    id: 5,
-    img: "/images/travelly.png",
-    link: "https://travelly-adventure.netlify.app/",
-    name: "Travelly",
-  },
+	{
+		id: 4,
+		img: "/images/web.png",
+		link: "https://webhose-cloud-hosting.netlify.app/index.html",
+		name: "Webhose.",
+	},
+
+	{
+		id: 5,
+		img: "/images/travelly.png",
+		link: "https://travelly-adventure.netlify.app/",
+		name: "Travelly",
+	},
+	{
+		id: 6,
+		img: "/images/pizza.png",
+		link: "https://pizzas-restro.herokuapp.com/",
+		name: "React-Pizza",
+	},
 ];
